5bffaaa0cb439d1eeb536277b0de81634e2543aa
fraciah <karfraciah@gmail.com>
2024-02-06 15:22:37 +0300

    Update CSS styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,11 +6,7 @@ const Navbar = () => {
         <nav>
             <div className='nav-left'>
                 <img src={gigitise} style={{width:'3rem'}} alt="logo" />
-                {/* <h1>Flexypro</h1> */}
             </div>
-            {/* <p>
-                Welcome to Flexypro, your ultimate solution hub for assignments across perse fields like essays, programming, engineering, accounting, dissertations, and more.
-            </p>     */}
             <div className='nav-right'>
                 <div className='search-bar'>
                     <input type="text" placeholder='Search for Essays'/>
