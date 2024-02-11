51e0da837743f965289a09784cc85a3f220efe70
fraciah <karfraciah@gmail.com>
2024-02-11 15:02:40 +0300

    Add search responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,11 @@
 import gigitise from "../../../public/gigitise.svg";
 import "./navbar.css";
 import { CiSearch } from "react-icons/ci";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [searchVisible, setSearchVisible] = useState(false);
   return (
     <nav>
       <div className="nav-left">
@@ -10,10 +13,11 @@ const Navbar = () => {
         <h1>Gigitise</h1>
       </div>
       <div className="search-icon">
-        <CiSearch size={30} />
+        {searchVisible && <MdKeyboardArrowLeft size={30} onClick={() => setSearchVisible(false)}/>}
+        <CiSearch size={30} onClick={() => setSearchVisible(true)}/>
       </div>
       <div className="nav-right">
-        <div className="search-bar">
+        <div className={`search-bar ${searchVisible ? 'visible' : 'search-bar'}`}>
           <input type="text" placeholder="Search for Essays" />
         </div>
         <div className="nav-btns">
