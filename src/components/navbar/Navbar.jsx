e529200163ab2730810ab7078e135adb348bdecf
fraciah <karfraciah@gmail.com>
2024-02-12 01:02:24 +0300

    Update Navbar component to toggle search visibility

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,25 +2,27 @@ import gigitise from "../../../public/gigitise.svg";
 import "./navbar.css";
 import { CiSearch } from "react-icons/ci";
 import { MdKeyboardArrowLeft } from "react-icons/md";
+
 import { useState } from "react";
 
 const Navbar = () => {
-  const [searchVisible, setSearchVisible] = useState(false);
+  const [searchVisible, setSearchVisible] = useState(true);
+
   return (
     <nav>
-      <div className="nav-left">
+      <div className={`nav-left ${searchVisible? '' : 'visible'}`}>
         <img src={gigitise} style={{ width: "3rem" }} alt="logo" />
         <h1>Gigitise</h1>
       </div>
       <div className="search-icon">
-        {searchVisible && <MdKeyboardArrowLeft size={30} onClick={() => setSearchVisible(false)}/>}
-        <CiSearch size={30} onClick={() => setSearchVisible(true)}/>
+        {searchVisible && <MdKeyboardArrowLeft size={30} onClick={() => setSearchVisible(!searchVisible)}/>}
+        <CiSearch size={30} onClick={() => setSearchVisible(!searchVisible)}/>
       </div>
       <div className="nav-right">
-        <div className={`search-bar ${searchVisible ? 'visible' : 'search-bar'}`}>
+        <div className={`search-bar ${searchVisible ? '' : 'mobile-invisible'}`}>
           <input type="text" placeholder="Search for Essays" />
         </div>
-        <div className="nav-btns">
+        <div className={`nav-btns ${searchVisible? '' : 'visible'}`}>
           <button>Sign Up</button>
           <button>Login</button>
         </div>
