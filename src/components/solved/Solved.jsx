97fbe00bfe9db672754405cc359ab2a7c31b2347
josephmusya <muciajoe@gmail.com>
2024-02-13 01:53:24 +0300

    Add: New service

---
+++ src/components/solved/Solved.jsx
@@ -0,0 +1,32 @@
+import { SlBadge } from "react-icons/sl";
+
+const Solved = () => {
+  return (
+    <div className="completed">
+      <section>
+        <dl className="new">NEW</dl>
+        <h1 className="intro__heading">
+          Want just to get already completed gigs?
+        </h1>
+        <h2>We got you covered!</h2>
+        <div className="desc">
+          <article className="mx-auto mt-4 max-w-md text-gray-500">
+            Check our archive library for already completed assignments, essays,
+            gigs for you. You only pick what you need!
+          </article>
+        </div>
+        <div className="badge">
+          <article>
+            <span className="strike-price">30%</span> OFF
+          </article>
+          <div>
+            <SlBadge className="badge-icon" size={200} />
+          </div>
+        </div>
+        <button className="btn--blue_220 w-button">Check Library</button>
+      </section>
+    </div>
+  );
+};
+
+export default Solved;
